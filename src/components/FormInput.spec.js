1f344920adcbd2b36730579eeead805fd9ac3854
mariusespejo <espejo.marius@gmail.com>
2017-10-12 14:25:09 -0700

    adding jest testing support, also travis config for CI

--- src/components/FormInput.spec.js
+++ src/components/FormInput.spec.js
@@ -1,52 +1,49 @@
-// import React from 'react';
-// import { shallow, mount } from 'enzyme';
-// import FormInput from './FormInput';
-// import { Form, Label, Input } from 'semantic-ui-react';
+import React from 'react';
+import FormInput from './FormInput';
+import { Form, Label, Input } from 'semantic-ui-react';
 
-// let props;
+let props;
 
-// beforeEach(() => {
-//   props = {
-//     input: {
-//       onChange: jest.fn(),
-//       name: 'name',
-//       onBlur: jest.fn(),
-//       value: 'value'
-//     },
-//     meta: { error: undefined, touched: false }
-//   };
-// });
+beforeEach(() => {
+  props = {
+    input: {
+      onChange: jest.fn(),
+      name: 'name',
+      onBlur: jest.fn(),
+      value: 'value'
+    },
+    meta: { error: undefined, touched: false }
+  };
+});
 
-// it('should render a form field input which calls onChange/onBlur from props', () => {
-//   const wrapper = shallow(<FormInput {...props} />);
+it('should render a form field input which calls onChange/onBlur from props', () => {
+  const wrapper = shallow(<FormInput {...props} />);
 
-//   expect(wrapper.find(Form.Field).length).toEqual(1);
-//   expect(wrapper.find(Input).length).toEqual(1);
-//   expect(wrapper.find(Label).length).toEqual(0);
-//   wrapper.find(Input).simulate('focus');
-//   wrapper.find(Input).simulate('change', { target: { value: '' } });
-//   wrapper.find(Input).simulate('blur');
-//   expect(props.input.onChange).toHaveBeenCalled();
-//   expect(props.input.onBlur).toHaveBeenCalled();
-// });
+  expect(wrapper.find(Form.Field).length).toEqual(1);
+  expect(wrapper.find(Input).length).toEqual(1);
+  expect(wrapper.find(Label).length).toEqual(0);
+  wrapper.find(Input).simulate('focus');
+  wrapper.find(Input).simulate('change', { target: { value: '' } });
+  wrapper.find(Input).simulate('blur');
+  expect(props.input.onChange).toHaveBeenCalled();
+  expect(props.input.onBlur).toHaveBeenCalled();
+});
 
-// it('should render a form field input with error message if touched and has error', () => {
-//   const metaProp = { error: 'Required', touched: true };
-//   const wrapper = mount(<FormInput {...props} meta={metaProp} />);
+it('should render a form field input with error message if touched and has error', () => {
+  const metaProp = { error: 'Required', touched: true };
+  const wrapper = mount(<FormInput {...props} meta={metaProp} />);
 
-//   expect(wrapper.find(Form.Field).length).toEqual(1);
-//   expect(wrapper.find(Input).length).toEqual(1);
-//   expect(wrapper.find(Label).length).toEqual(1);
-//   expect(wrapper.find(Label).text()).toEqual(metaProp.error);
-// });
+  expect(wrapper.find(Form.Field).length).toEqual(1);
+  expect(wrapper.find(Input).length).toEqual(1);
+  expect(wrapper.find(Label).length).toEqual(1);
+  expect(wrapper.find(Label).text()).toEqual(metaProp.error);
+});
 
-// it('should render a simple value string if read only', () => {
-//   const wrapper = shallow(<FormInput {...props} readOnly />);
+it('should render a simple value string if read only', () => {
+  const wrapper = shallow(<FormInput {...props} readOnly />);
 
-//   expect(wrapper.find(Form.Field).length).toEqual(0);
-//   expect(wrapper.find(Input).length).toEqual(0);
-//   expect(wrapper.find(Label).length).toEqual(0);
-//   expect(wrapper.find('.form-field-read-only').text()).toEqual(
-//     props.input.value
-//   );
-// });
+  expect(wrapper.find(Form.Field).length).toEqual(0);
+  expect(wrapper.find(Input).length).toEqual(0);
+  expect(wrapper.find(Label).length).toEqual(0);
+  expect(wrapper.find('.read-only').text()).toEqual(props.input.value);
+});
---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+console.error = message => {
+  throw new Error(message);
+};
