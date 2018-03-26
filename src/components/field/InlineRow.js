4163f5223e63e99bc859d86cb1608cefd5b6346f
mariusespejo <espejo.marius@gmail.com>
2018-03-26 14:36:05 -0700

    add label text align configuration support

--- src/components/field/InlineRow.js
+++ src/components/field/InlineRow.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 
-const InlineRow = ({ label, children, width, value, labelVerticalAlign }) => (
+const InlineRow = ({
+  label,
+  children,
+  width,
+  value,
+  labelVerticalAlign,
+  labelTextAlign
+}) => (
   <Grid.Row style={{ paddingTop: 5, paddingBottom: 5 }}>
     <Grid.Column
       className="form-row-label"
       width={width || 5}
       verticalAlign={labelVerticalAlign || 'middle'}
-      textAlign="right"
+      textAlign={labelTextAlign || 'right'}
     >
       <strong>{label}</strong>
     </Grid.Column>
